150aa32defe0bf96de779aa163dd580c4dae6630
Filip.kostadinovski <72503214+fichomacedon@users.noreply.github.com>
2023-12-02 19:31:52 +0100

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+let priceOfOnePhone = 119.95;
+let fivePrecentTax = (119.95 / 100) * 5;
+console.log(fivePrecentTax);
+let priceOfOnePhoneTaxIncluded = priceOfOnePhone + fivePrecentTax;
+console.log(priceOfOnePhoneTaxIncluded);
+let priceOfThirtyPhones = priceOfOnePhoneTaxIncluded * 30;
+console.log(priceOfThirtyPhones);
