f62189f5eb53011c2d9bbe9000afcc3a3067f315
Filip.kostadinovski <72503214+fichomacedon@users.noreply.github.com>
2023-12-10 15:03:08 +0100

    Add files via upload

---
+++ JS/ageCalculator/app.js
@@ -0,0 +1,10 @@
+console.log("script is working");
+function calculateAge(birthYear, currentYear) {
+  let result = currentYear - birthYear;
+  console.log("You are " + result + " years old");
+}
+calculateAge(1992, 2023);
+calculateAge(1996, 2023);
+calculateAge(1993, 2023);
+
+// The Age Calculator
